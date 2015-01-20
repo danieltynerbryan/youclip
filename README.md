f77d70333dc6f932ec0da56d648f8d656c2d1349
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2015-01-20 14:01:06 -0500

    small organizational changes and a readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# YouClip
+
+A simple editor for Youtube videos.
+
+## Requirements
+
+1. Ruby version 2.0 or higher
+1. Rails version 4.0 or higher
+
+## Getting Started
+
+1. git clone https://github.com/danieltynerbryan/youclip.git
+1. bundle install
+1. rake db:migrate
--- app/assets/javascripts/clips.js
+++ app/assets/javascripts/clips.js
@@ -1,9 +1,13 @@
-function getVideo() {
-	videoURL = $('#video-id').val();
-	regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-	match = videoURL.match(regExp);
-	videoID = match[7];
-	if (videoID != '') {
-		window.location.href = 'youtube/'+videoID;
-	}
-}
+$(document).ready(function(){
+	
+	$('#new-video-button').click(function() {
+		videoURL = $('#video-id').val();
+		regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+		match = videoURL.match(regExp);
+		videoID = match[7];
+		if (videoID != '') {
+			window.location.href = 'youtube/'+videoID;
+		}
+	})
+
+})
